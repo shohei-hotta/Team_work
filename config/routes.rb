20cb2dc0679a86e58c304c716a5a53c956204750
shohei-hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-04-27 06:55:37 +0000

    ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   resources :users
-  get 'blogs/new'
-  get 'blogs/edit'
-  get 'blogs/show'
-  get 'blogs/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "blogs#index"
+  resources :blogs
 end
