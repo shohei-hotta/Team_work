0a4387c1cdb78cba3fb7395fab76d5ac30f3bdf0
taroosuga <copernix0@gmail.com>
2020-04-27 16:46:37 +0900

    session機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: "blogs#index"
   resources :blogs
   resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
 end
