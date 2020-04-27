d0ef72600e5b7ea397c331cc8919f56efa2e1a21
shohei-hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-04-27 09:32:58 +0000

    再度コンフリクト解消

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,13 @@
+class BlogsController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :users
+  get 'blogs/new'
+  get 'blogs/edit'
+  get 'blogs/show'
+  get 'blogs/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
