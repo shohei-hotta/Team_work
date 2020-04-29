538e453ca35d0ec04a24a516000206dd4af66c7c
taroosuga <copernix0@gmail.com>
2020-04-29 14:31:50 +0900

    user編集機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "blogs#index"
   resources :blogs
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show, :edit]
   resources :sessions, only: [:new, :create, :destroy]
 end
