d9630d30517c5e0d68e5239af86d67f7e5c37262
taroosuga <copernix0@gmail.com>
2020-04-30 09:54:26 +0900

    ルーティング編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "blogs#index"
   resources :blogs
-  resources :users, only: [:new, :create, :show, :edit]
+  resources :users, only: [:new, :create, :show, :edit, :update]
   resources :sessions, only: [:new, :create, :destroy]
 end
